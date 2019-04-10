44167527a59064449f4fc55d434a0888dadb3751
2226171237 <2226171237@qq.com>
2019-04-10 15:59:12 +0800

    4-10

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@
 INPUT_DIR=$1
 OUTPUT_DIR=$2
 
-python attack.py \
+python attack_inception_v1_test.py \
   --input_dir="${INPUT_DIR}" \
   --output_dir="${OUTPUT_DIR}" \
   --checkpoint_path=./models/inception_v1/inception_v1.ckpt
