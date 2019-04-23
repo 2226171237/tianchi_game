9e711324ac9a9a2ff1394537f4129e242626a6f6
2226171237 <2226171237@qq.com>
2019-04-23 14:20:36 +0800

    eps=40/255.0

--- attack.py
+++ attack.py
@@ -226,7 +226,7 @@ def main(_):
             parse_params(eps=0.3, eps_iter=0.06, nb_iter=10, y=None, ord=inf, decay_factor=1.0,
                clip_min=None, clip_max=None, y_target=None, sanity_checks=True, **kwargs)
             '''
-            attack_params = {"eps": 25.0 / 255.0, "eps_iter": 0.01, "clip_min": -1.0, "clip_max": 1.0, \
+            attack_params = {"eps": 40.0 / 255.0, "eps_iter": 0.01, "clip_min": -1.0, "clip_max": 1.0, \
                              "nb_iter": 15, "decay_factor": 1.0, "y_target": one_hot_target_class}
             
             x_adv = mim.generate(x_input, **attack_params)
